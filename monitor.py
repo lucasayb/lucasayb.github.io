41e18b85c7883ecf28f00a272e6182b96f647f70
Lucas Yamamoto <17356081+lucasayb@users.noreply.github.com>
2025-05-18 00:20:34 -0400

    some fixes

--- monitor.py
+++ monitor.py
@@ -1,21 +1,23 @@
 import requests
 import io
 import json
+import time
+
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
 ua = UserAgent()
 
-def search_ebay(keyword, exclude, price_max=210.0):
+def search_ebay(keyword, exclude, price_max=210.0, page=1):
     headers = {'User-Agent': ua.random}
     query = keyword.replace(" ", "+")
-    url = f"https://www.ebay.ca/sch/i.html?_nkw={query}&_sop=15&_pgn=1&_dcat=139973&Region%2520Code=%21%7CRegion%2520Free%7CNTSC%252DU%252FC%2520%2528US%252FCanada%2529%7CPAL&LH_BIN=1&_sop=15"  # Sort by Price + Shipping: Lowest First
+    url = f"https://www.ebay.ca/sch/i.html?_nkw={query}&_sop=15&_pgn=1&_dcat=139973&Region%2520Code=%21%7CRegion%2520Free%7CNTSC%252DU%252FC%2520%2528US%252FCanada%2529%7CPAL&LH_BIN=1&_sop=15&_ipg=240&_pgn={page}"  # Sort by Price + Shipping: Lowest First
 
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.text, "html.parser")
 
     results = []
-
+    
     for item in soup.select(".s-item"):
         title = item.select_one(".s-item__title")
         price = item.select_one(".s-item__price")
@@ -23,7 +25,7 @@ def search_ebay(keyword, exclude, price_max=210.0):
 
         if title and price and link:
             for query_to_exclude in exclude:
-                if query_to_exclude in title:
+                if query_to_exclude.lower() in title.get_text().lower():
                     continue
 
             try:
@@ -41,8 +43,6 @@ def search_ebay(keyword, exclude, price_max=210.0):
                 continue
 
     return results
-
-
 
 
 queries = [
@@ -65,12 +65,13 @@ queries = [
 
 listings = {}
 
-for query in queries:
-    results = search_ebay(query['query'], query["exclude"], query["price_max"])
-    listings[query["query"]] = results
-
+for page in range(1, 2):
+    for query in queries:
+        results = search_ebay(query['query'], query["exclude"], query["price_max"], page)
+        listings[query["query"]] = results
+        time.sleep(2)
 
 listings_in_json = json.dumps(listings)
 
-with io.open("./docs/ebay-listing.json", "w+") as file:
+with io.open("./ebay-listing.json", "w+") as file:
     file.write(listings_in_json)
